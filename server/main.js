94e00e6522df3a680ba5c6a5a285f8adb9ba0ba4
Shawn Sutherland <shawncsutherland@gmail.com>
2017-09-10 11:00:25 -0600

    Simple server

---
+++ server/main.js
@@ -0,0 +1,19 @@
+const net = require("net");
+const port = 3000;
+
+const server = net.createServer(function(socket)
+{
+	console.log("connected");
+	
+	socket.on("end", function()
+	{
+		console.log("disconnected");
+	});
+});
+
+server.on("error", function(error)
+{
+	throw error;
+});
+
+server.listen(3000);
